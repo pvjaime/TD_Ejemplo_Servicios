14680bed9af96cf3838cc1439650ceade91796e0
Jaime Perez Varas <jaime.perez.varas@gmail.com>
2024-05-14 20:39:55 -0400

    Termino del ServicioDeTest

--- app/src/main/java/perez/jaime/tdejemploservicios/ServicioDeTest.kt
+++ app/src/main/java/perez/jaime/tdejemploservicios/ServicioDeTest.kt
@@ -2,11 +2,38 @@ package perez.jaime.tdejemploservicios
 
 import android.app.Service
 import android.content.Intent
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
 
 class ServicioDeTest : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onBind(intent: Intent): IBinder? {
+        // Implementar si es un servicio vinculado
+        return null
     }
+
+    //En esta funcion es donde el servicio comienza
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        //Aca iria toda la logica que quiero hacer
+        val timer = object: CountDownTimer(20000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("El servicio", "Tic Tac")
+            }
+
+            override fun onFinish() {
+                Log.d("El servicio", "Termino el ContDown")
+            }
+        }
+        timer.start()
+
+        return START_STICKY
+    }
+
+    //Cuando se destruya el servicio
+    override fun onDestroy() {
+        Log.d("El servicio", "Hizo caputttt")
+        super.onDestroy()
+    }
+
 }
