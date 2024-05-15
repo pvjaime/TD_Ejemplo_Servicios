860966c13b1ea94ddefa35a8d93d97b12f622c78
Jaime Perez Varas <jaime.perez.varas@gmail.com>
2024-05-14 20:28:56 -0400

    Initial commit

---
+++ app/src/main/java/perez/jaime/tdejemploservicios/ServicioDeTest.kt
@@ -0,0 +1,12 @@
+package perez.jaime.tdejemploservicios
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ServicioDeTest : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
